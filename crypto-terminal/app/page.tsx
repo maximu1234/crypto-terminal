297c62d18873ef475d84d4aeff51a960938d91ca
maximu1234 <128886718+maximu1234@users.noreply.github.com>
2026-05-15 22:50:25 +0700

    Delete crypto-terminal directory

--- crypto-terminal/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import GridLayout from "@/components/layout/GridLayout";
-
-export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-[#0b1220] p-4">
-      <GridLayout />
-    </main>
-  );
-}

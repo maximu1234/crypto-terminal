f6b6790b19bfc9d557c0f973792aa5ac3fa9923e
maximu1234 <128886718+maximu1234@users.noreply.github.com>
2026-05-16 22:23:35 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div
+      style={{
+        background: "#0b1220",
+        color: "white",
+        minHeight: "100vh",
+        padding: "40px",
+        fontSize: "32px",
+      }}
+    >
+      CRYPTO TERMINAL WORKS 🚀
+    </div>
+  );
+}
